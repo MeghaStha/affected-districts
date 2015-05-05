d6513ba2b95acbd7d4fda9d607d16533c27ad668
looza <meghashrestha2@gmail.com>
2015-05-05 14:53:18 +0545

    intensity maps

---
+++ js/hh.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+var map = new L.map('map').setView([27.8006, 85.3934],8);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18
+}).addTo(map);
+L.tileLayer('https://{s}.tiles.mapbox.com/v4/kll.jkmrala9/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia2xsIiwiYSI6IktVRUtfQnMifQ.GJAHJPvusgK_f0NsSXS8QA',{
+		maxZoom: 18
+}).addTo(map);
+
+
+
+});
