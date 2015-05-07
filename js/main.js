58a07f005bc3dd9a59a6e72f605e5b845145eacf
looza <meghashrestha2@gmail.com>
2015-05-07 14:23:18 +0545

    applied transparency

--- js/main.js
+++ js/main.js
@@ -4,35 +4,33 @@ var map = new L.map('map').setView([27.8006, 85.3934],8);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18
 }).addTo(map);
-L.tileLayer('https://{s}.tiles.mapbox.com/v4/kll.m3i8i8ii/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia2xsIiwiYSI6IktVRUtfQnMifQ.GJAHJPvusgK_f0NsSXS8QA',{
-		maxZoom: 18
-}).addTo(map);
+var layers = [
+ L.tileLayer('https://{s}.tiles.mapbox.com/v4/kll.m3i8i8ii/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia2xsIiwiYSI6IktVRUtfQnMifQ.GJAHJPvusgK_f0NsSXS8QA',{transparent : true, opacity : 0.8}),
+ L.tileLayer ('https://{s}.tiles.mapbox.com/v4/kll.m3j32hkl/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia2xsIiwiYSI6IktVRUtfQnMifQ.GJAHJPvusgK_f0NsSXS8QA',{transparent : true, opacity : 0.8}),
+  L.tileLayer ('https://{s}.tiles.mapbox.com/v4/kll.jkmrala9/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia2xsIiwiYSI6IktVRUtfQnMifQ.GJAHJPvusgK_f0NsSXS8QA',{transparent : true, opacity : 0.8})
+];
+var layerGroup = L.layerGroup().addTo(map);
+layerGroup.clearLayers().addLayer(layers[0]);
 var epicenterIcon = L.icon({
     iconUrl: 'js/image/epicenter.png',
-    iconSize:     [30, 28], // size of the icon
+    iconSize: [30, 28], // size of the icon
    
  });
  L.marker([28.210368, 84.742003], {icon: epicenterIcon}).addTo(map);
 
-    $("#intSubmit").click(function(){
- L.tileLayer('https://{s}.tiles.mapbox.com/v4/kll.m3i8i8ii/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia2xsIiwiYSI6IktVRUtfQnMifQ.GJAHJPvusgK_f0NsSXS8QA',{
-		maxZoom: 18
-}).addTo(map);
- $("#legend").html("<img src='images/intensity.PNG' width ='160px' height= '150px'/>");
-
-    }); 
+ $("#intSubmit").click(function(){
+var inti = layerGroup.clearLayers().addLayer(layers[0]);
+$("#legend").html("<img src='images/intensity.PNG' width ='160px' height= '150px'/>");
+  }); 
+  
 $("#popSubmit").click(function(){
-L.tileLayer ('https://{s}.tiles.mapbox.com/v4/kll.m3j32hkl/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia2xsIiwiYSI6IktVRUtfQnMifQ.GJAHJPvusgK_f0NsSXS8QA',{
-		maxZoom: 18
-		}).addTo(map);
-	$("#legend").html("<img src='images/pop.PNG' width ='160px' height= '150px'/>");
-});
-$("#hhSubmit").click(function(){
-L.tileLayer ('https://{s}.tiles.mapbox.com/v4/kll.jkmrala9/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia2xsIiwiYSI6IktVRUtfQnMifQ.GJAHJPvusgK_f0NsSXS8QA',{
-		maxZoom : 18
-}).addTo(map);
-$("#legend").html("<img src='images/hh.PNG' width = '160px' height= '150px'/>");
+var pop = layerGroup.clearLayers().addLayer(layers[1]);
+$("#legend").html("<img src='images/pop.PNG' width ='160px' height= '150px'/>");
 
+	});
+$("#hhSubmit").click(function(){
+var hh = layerGroup.clearLayers().addLayer(layers[2]);
+$("#legend").html("<img src='images/hh.PNG' width = '160px' height= '150px'/>");
 });
 
 });
